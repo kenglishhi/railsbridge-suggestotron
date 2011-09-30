5515a5d0c2b4774512c226fc70598f282250c0cf
Kevin English <kevin.english@riotgames.com>
2011-09-29 18:33:55 -0700

    CoffeeScript Part1

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,8 +1,13 @@
 class VotesController < ApplicationController
   def create
-    topic = Topic.find(params[:topic_id])
-    topic.votes << Vote.create
-    topic.save
-    redirect_to topics_path
+    @topic = Topic.find(params[:topic_id])
+    @topic.votes << Vote.create
+    @topic.save
+    respond_to do |format|
+      format.html { redirect_to topics_path }
+      format.json { render json: @topic }
+    end
+
+
   end
 end
